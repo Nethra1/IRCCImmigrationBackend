938d546e1d3e925497641381afaefad3b7c323b7
Nethra1 <vijayadasnethra97@gmail.com>
2022-07-19 17:12:46 -0400

    IRCC API

--- api_mongo.py
+++ api_mongo.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 CORS(app)
 
 database_name = "API"
-DB_URI = "mongodb+srv://{}:{}@cluster0.91ztf.mongodb.net/{}?retryWrites=true&w=majority".format("adtproject", "irccimmigration", database_name)
+DB_URI = "mongodb+srv://irccimmigration:adtproject@cluster0.91ztf.mongodb.net/API?retryWrites=true&w=majority"
+# //.format("newuser", "newuser", database_name)
 app.config["MONGODB_HOST"] = DB_URI
 db = MongoEngine()
 db.init_app(app)
@@ -31,13 +32,16 @@ def loginUser():
     dbuser = User.objects(uname=user['uname'], password=user['password']).first()
     print(dbuser['uname'])
     return make_response("test", 201)
+    # print("success")
+    # return make_response("test", 201)
+
 
 
 @app.route("/ircc/signup", methods = ["POST"])
 def signupUser():
     user = request.get_json()
     print(user)
-    pw_hash = bcrypt.generate_password_hash(user['password'])
+    # pw_hash = bcrypt.generate_password_hash(user['password'])
     data = User(uname=user['uname'], password=str(user['password']))
     data.save()
     print(user)
