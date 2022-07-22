68779c784c70de6637fcad3240b6fa77351f9996
Nethra1 <vijayadasnethra97@gmail.com>
2022-07-21 21:14:56 -0400

    deploy

--- api_mongo.py
+++ api_mongo.py
@@ -174,9 +174,9 @@ def uploadFiles():
     # save the file
     return make_response("", 201)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
