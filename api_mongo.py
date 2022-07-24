2e1aa6e950561606abacf9cd48b1f7d607cafc8c
Nethra1 <vijayadasnethra97@gmail.com>
2022-07-24 19:52:29 -0400

    deploy

--- api_mongo.py
+++ api_mongo.py
@@ -1,7 +1,6 @@
 from crypt import methods
 import os
 from flask import Flask, jsonify, make_response, render_template, request
-# from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_mongoengine import MongoEngine
 from mongoengine import *
